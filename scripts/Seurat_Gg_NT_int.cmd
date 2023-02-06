0b43c01d679f0f83fa09000b3324ec5c8e79fd2e
safabio <fabio.sacher@bluewin.ch>
2023-02-06 15:18:20 +0100

    correct file paths scripts -> markdown

--- scripts/Seurat_Gg_NT_int.cmd
+++ scripts/Seurat_Gg_NT_int.cmd
@@ -1,3 +1,3 @@
-Rscript -e rmarkdown::render('~/spinal_cord_paper/scripts/Gg_ctrl_int.Rmd')
-Rscript -e rmarkdown::render('~/spinal_cord_paper/scripts/Gg_lumb_int.Rmd')
-Rscript -e rmarkdown::render('~/spinal_cord_paper/scripts/Gg_poly_int.Rmd')
+Rscript -e rmarkdown::render('~/spinal_cord_paper/markdown/Gg_ctrl_int.Rmd')
+Rscript -e rmarkdown::render('~/spinal_cord_paper/markdown/Gg_lumb_int.Rmd')
+Rscript -e rmarkdown::render('~/spinal_cord_paper/markdown/Gg_poly_int.Rmd')
