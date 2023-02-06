d97824a96bfffb67f566e7b90c9d64d6c35a840c
safabio <fabio.sacher@bluewin.ch>
2023-02-06 18:54:09 +0100

    increase memory

--- scripts/Seurat_Gg_NT_int_array.sh
+++ scripts/Seurat_Gg_NT_int_array.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=Seurat_Gg_NT_int_array
 #SBATCH --time=00:30:00
 #SBATCH --qos=30min
-#SBATCH --mem=20G
+#SBATCH --mem=30G
 #SBATCH --mail-user=fabio.sacher@unibas.ch
 #SBATCH --mail-type=ALL
 #SBATCH --output ./slurm_out/%x-%A_%a.out
