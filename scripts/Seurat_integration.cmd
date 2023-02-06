2c3176f24e2fae8dff72ad3b0fb7e186dd744d7b
safabio <fabio.sacher@bluewin.ch>
2023-02-06 18:11:49 +0100

    prepare integration files

--- scripts/Seurat_integration.cmd
+++ scripts/Seurat_integration.cmd
@@ -1,3 +1,4 @@
 Rscript -e rmarkdown::render('~/spinal_cord_paper/markdowm/Integration_Gg_NT_ctrl_1_and_2.Rmd')
 Rscript -e rmarkdown::render('~/spinal_cord_paper/markdowm/Integration_Gg_NT_lumb_1_and_2.Rmd')
 Rscript -e rmarkdown::render('~/spinal_cord_paper/markdowm/Integration_Gg_NT_poly_1_and_2.Rmd')
+Rscript -e rmarkdown::render('~/spinal_cord_paper/markdowm/Integration_Gg_NT_D05_D07_ctrl_1.Rmd')
