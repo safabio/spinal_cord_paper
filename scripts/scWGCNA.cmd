465df177568360c0a0f7f2ad7633420c3b730081
safabio <fabio.sacher@bluewin.ch>
2023-02-07 15:34:55 +0100

    prepare scWGCNA scripts for array on ctrl_int, lumb_int, poly_int, and devel_int

---
+++ scripts/scWGCNA.cmd
@@ -0,0 +1,4 @@
+Rscript -e rmarkdown::render('~/spinal_cord_paper/markdown/scWGCNA_Gg_ctrl_int.Rmd')
+Rscript -e rmarkdown::render('~/spinal_cord_paper/markdown/scWGCNA_Gg_lumb_int.Rmd')
+Rscript -e rmarkdown::render('~/spinal_cord_paper/markdown/scWGCNA_Gg_poly_int.Rmd')
+Rscript -e rmarkdown::render('~/spinal_cord_paper/markdown/scWGCNA_Gg_devel_int.Rmd')
---
+++ scripts/scWGCNA_array.sh
@@ -0,0 +1,18 @@
+!/bin/bash
+
+
+#SBATCH --job-name=scWGCNA_ctrl_lumb_poly_and_devel_int
+#SBATCH --time=01:00:00
+#SBATCH --qos=6hours
+#SBATCH --mem=80G
+#SBATCH --mail-user=fabio.sacher@unibas.ch
+#SBATCH --mail-type=ALL
+#SBATCH --output ./slurm_out/%x-%A_%a.out
+#SBATCH --array=1-4
+
+
+module purge
+ml load R/4.1.0-foss-2018b
+ml Pandoc/2.7.3
+
+$(head -$SLURM_ARRAY_TASK_ID scWGCNA.cmd | tail -1) 
