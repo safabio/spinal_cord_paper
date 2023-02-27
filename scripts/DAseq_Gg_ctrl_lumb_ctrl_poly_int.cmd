1e6e65ea9701a29ce68ef6c0749a1e9236126a60
safabio <fabio.sacher@bluewin.ch>
2023-02-27 09:35:52 +0100

    prepare DAseq differential abundance analysis scripts

---
+++ scripts/DAseq_Gg_ctrl_lumb_ctrl_poly_int.cmd
@@ -0,0 +1,2 @@
+Rscript -e rmarkdown::render('../markdown/DAseq_Gg_ctrl_lumb_int.Rmd')
+Rscript -e rmarkdown::render('../markdown/DAseq_Gg_ctrl_poly_int.Rmd')
---
+++ scripts/DAseq_Gg_ctrl_lumb_ctrl_poly_int_array.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#SBATCH --job-name=DAseq_Gg_ctrl_lumb_ctrl_poly_int_array
+#SBATCH --time=06:00:00
+#SBATCH --qos=6hours
+#SBATCH --mem=80G
+#SBATCH --mail-user=fabio.sacher@unibas.ch
+#SBATCH --mail-type=ALL
+#SBATCH --output ./slurm_out/%x-%A_%a.out
+#SBATCH --array=1-2
+
+
+module purge
+ml load R/4.1.0-foss-2018b
+ml Pandoc/2.7.3
+
+$(head -$SLURM_ARRAY_TASK_ID DAseq_Gg_ctrl_lumb_ctrl_poly_int.cmd | tail -1) 
