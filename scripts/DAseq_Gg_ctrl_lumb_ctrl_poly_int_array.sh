12399485a54788b029284f457c6b37b977a83196
safabio <fabio.sacher@bluewin.ch>
2023-02-28 09:08:47 +0100

    update sbatch parameters

--- scripts/DAseq_Gg_ctrl_lumb_ctrl_poly_int_array.sh
+++ scripts/DAseq_Gg_ctrl_lumb_ctrl_poly_int_array.sh
@@ -2,9 +2,9 @@
 
 
 #SBATCH --job-name=DAseq_Gg_ctrl_lumb_ctrl_poly_int_array
-#SBATCH --time=06:00:00
+#SBATCH --time=01:00:00
 #SBATCH --qos=6hours
-#SBATCH --mem=80G
+#SBATCH --mem=15G
 #SBATCH --mail-user=fabio.sacher@unibas.ch
 #SBATCH --mail-type=ALL
 #SBATCH --output ./slurm_out/%x-%A_%a.out
