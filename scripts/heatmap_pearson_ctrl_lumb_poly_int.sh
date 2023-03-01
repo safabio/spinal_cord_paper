6374d99e1fb81db3a3066f80d6fa21e6509e0577
safabio <fabio.sacher@bluewin.ch>
2023-03-01 14:53:03 +0100

    bash script to run the ctrl vs lumb and ctrl vs poly heatmaps

---
+++ scripts/heatmap_pearson_ctrl_lumb_poly_int.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#SBATCH --job-name=heatmap_pearson_ctrl_lumb_poly_int_render
+#SBATCH --time=00:02:00
+#SBATCH --qos=30min
+#SBATCH --mem=10G
+#SBATCH --mail-user=fabio.sacher@unibas.ch
+#SBATCH --mail-type=ALL
+#SBATCH --output ./slurm_out/%x-%j.out
+
+
+module purge
+ml load R/4.1.0-foss-2018b
+ml Pandoc/2.7.3
+
+Rscript -e "rmarkdown::render('~/spinal_cord_paper/markdown/heatmap_pearson_ctrl_lumb_poly_int.Rmd')"
