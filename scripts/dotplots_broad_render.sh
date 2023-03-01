9a4f5e9607d8fd3d61fe58a1a6faa7f2ecfd13c9
safabio <fabio.sacher@bluewin.ch>
2023-03-01 13:19:56 +0100

    dotplots of broad cluster markers

---
+++ scripts/dotplots_broad_render.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#SBATCH --job-name=dotplots_broad_render
+#SBATCH --time=00:05:00
+#SBATCH --qos=30min
+#SBATCH --mem=20G
+#SBATCH --mail-user=fabio.sacher@unibas.ch
+#SBATCH --mail-type=ALL
+#SBATCH --output ./slurm_out/%x-%j.out
+
+
+module purge
+ml load R/4.1.0-foss-2018b
+ml Pandoc/2.7.3
+
+Rscript -e "rmarkdown::render('~/spinal_cord_paper/markdown/dotplots_broad.Rmd')"
