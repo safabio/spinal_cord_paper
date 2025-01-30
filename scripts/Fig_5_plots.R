980ceaef8b7b785e575068857f0b646fc0240f65
safabio <fabio.sacher@bluewin.ch>
2025-01-30 16:35:41 +0100

    flat volcanoplot for BPoly10int DE genes DE within cluster B vs Poly pvaladj as alpha or alpha and size

--- scripts/Fig_5_plots.R
+++ scripts/Fig_5_plots.R
@@ -3,16 +3,11 @@
 ## Fabio Sacher, 21.12.23
 ### ### ### ### ### ### ### ### ### ### ###
 
-
 library(Seurat)
-library(dplyr)
-library(stringr)
-library(ggplot2)
+library(tidyverse)
 library(gridExtra)
 library(magrittr)
-library(tibble)
-
-
+library(ggbeeswarm)
 
 ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 #### Compare ctrl and poly 1 & 2 FP/RP cluster size ####
@@ -88,3 +83,155 @@ pdf("~/spinal_cord_paper/figures/RP_FP_cluster_sizes.pdf", width = 7, height = 7
 p + theme_classic()
 dev.off()
 
+### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### BP10int DE genes flat volcanoplot (miloR insp) ####
+### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+# ### ### ### ### ### #
+# Do not run again. DE calculation takes a long time!
+# ### ### ### ### ### #
+
+# my.se <- readRDS("~/spinal_cord_paper/data/Gg_ctrl_poly_int_seurat_250723.rds")
+# # cluster labels from B10int and L10int
+# ctrl_poly_int_combined_labels <- readRDS("~/spinal_cord_paper/annotations/ctrl_poly_int_combined_labels.rds")
+# 
+# identical(colnames(my.se), rownames(ctrl_poly_int_combined_labels))
+# 
+# my.se$annot_sample  <- ctrl_poly_int_combined_labels$annot_sample
+# 
+# my.se@active.assay <- "RNA"
+# 
+# markers <- list()
+# numbers <- list()
+# composition <- list()
+# 
+# for (i in seq(levels(Idents(my.se)))) {
+#   # subset for individual clusters
+#   mn.se <- subset(x = my.se, idents = levels(Idents(my.se))[i])
+#   mn.se$sample <- str_extract(mn.se$orig.ident, "ctrl|poly")
+#   
+#   composition[[i]] <- mn.se[[]] %>% 
+#     select(sample, annot_sample)
+#   
+#   Idents(mn.se) <- "sample"
+#   
+#   tmp_markers <- FindMarkers(mn.se,
+#                              ident.1 = "ctrl",
+#                              only.pos = FALSE, 
+#                              min.pct = 0.25, 
+#                              logfc.threshold =  0.2,
+#                              latent.vars = c("CC.Difference.seurat"), 
+#                              test.use = "MAST", 
+#                              assay = "RNA")
+#   # cell numbers per sample
+#   numbers[[i]] <- data.frame(table(mn.se$sample))
+#   
+#   tmp_markers <- tmp_markers %>%
+#     rownames_to_column("Gene.stable.ID") %>% 
+#     left_join(gnames)
+#   
+#   
+#   markers[[i]] <- tmp_markers
+# }
+# 
+# names(markers) <- paste0("cl-", levels(Idents(my.se)))
+# names(numbers) <- paste0("cl-", levels(Idents(my.se)))
+# names(composition) <- paste0("cl-", levels(Idents(my.se)))
+# 
+# # bind lists into data frames
+# poly_markers <- bind_rows(markers, .id = "cluster") %>% 
+#   mutate(cluster = factor(cluster, levels = paste0("cl-", levels(Idents(my.se)))))
+# poly_numbers <- bind_rows(numbers, .id = "cluster") %>% 
+#   mutate(cluster = factor(cluster, levels = paste0("cl-", levels(Idents(my.se)))))
+# poly_composition <- bind_rows(composition, .id = "cluster") %>% 
+#   mutate(cluster = factor(cluster, levels = paste0("cl-", levels(Idents(my.se)))))
+# 
+# saveRDS(poly_markers, "~/spinal_cord_paper/data/Gg_ctrl_poly_int_markers.rds")
+# saveRDS(poly_numbers, "~/spinal_cord_paper/data/Gg_ctrl_poly_int_numbers.rds")
+# saveRDS(poly_composition, "~/spinal_cord_paper/data/Gg_ctrl_poly_int_composition.rds")
+
+poly_markers <- readRDS("~/spinal_cord_paper/data/Gg_ctrl_poly_int_markers.rds") %>% 
+  mutate(clust_id = str_remove(cluster, "^cl-")) %>% 
+  mutate(clust_id = factor(clust_id, levels = c(1:32))) %>% 
+  mutate(clust_id = fct_rev(clust_id)) %>% 
+  # filter for significant DEs
+  # p_val_adj < 0.05 & abs(avg_log2FC) > 0.5)
+  filter(p_val_adj < 0.05) %>% 
+  # filter(abs(avg_log2FC) > 0.5) %>% 
+  mutate(sample = case_when(
+    avg_log2FC > 0 ~ "B10int",
+    avg_log2FC < 0 ~ "Poly10int"
+  )) %>% 
+  # transform p_val_adj for alpha plotting
+  mutate(neg_log_10_p_val_adj = -log10(p_val_adj)) %>% 
+  # for plotting we treshold the pvals it
+  mutate(p_alpha = case_when(
+    neg_log_10_p_val_adj > 20 ~ 20,
+    TRUE ~ neg_log_10_p_val_adj
+  )) 
+
+
+
+#code taken and modified from miloR::plotDAbeeswarm()
+
+# group.by = NULL
+# subset.nhoods = NULL
+
+beeswarm_pos <- ggplot_build(
+  poly_markers %>% 
+    arrange(clust_id) %>%
+    ggplot(aes(clust_id, avg_log2FC)) +
+    geom_quasirandom()
+)
+
+pos_x <- beeswarm_pos$data[[1]]$x
+pos_y <- beeswarm_pos$data[[1]]$y
+n_groups <- length(levels(poly_markers$clust_id))
+
+alpha <- poly_markers %>% 
+  arrange(clust_id) %>% 
+  mutate(pos_x = pos_x, pos_y = pos_y) %>% 
+  # ggplot(aes(pos_x, pos_y, color = avg_log2FC)) +
+  ggplot(aes(pos_x, pos_y, color = sample, alpha = p_alpha)) +
+  scale_color_manual(values = c("black", "goldenrod3")) +
+  xlab("clust_id") +
+  ylab("Log Fold Change") + 
+  scale_alpha_continuous(range = c(0,1)) +
+  scale_x_continuous(breaks = seq(1, n_groups),
+                     labels = setNames(levels(poly_markers$clust_id), seq(1, n_groups))) +
+  geom_point(pch = 16) + 
+  ylim(c(-3,3)) +
+  coord_flip() + 
+  theme_bw(base_size = 22) +
+  theme(strip.text.y = element_text(angle = 0))
+
+alpha_size <- poly_markers %>% 
+  arrange(clust_id) %>% 
+  mutate(pos_x = pos_x, pos_y = pos_y) %>% 
+  # ggplot(aes(pos_x, pos_y, color = avg_log2FC)) +
+  ggplot(aes(pos_x, pos_y, color = sample, alpha = p_alpha, size = p_alpha)) +
+  scale_size(range = c(0.4,2)) +
+  scale_color_manual(values = c("black", "goldenrod3")) +
+  xlab("clust_id") +
+  ylab("Log Fold Change") + 
+  scale_alpha_continuous(range = c(0.1,1)) +
+  scale_x_continuous(breaks = seq(1, n_groups),
+                     labels = setNames(levels(poly_markers$clust_id), seq(1, n_groups))) +
+  geom_point(pch = 16) + 
+  ylim(c(-3,3)) +
+  coord_flip() + 
+  theme_bw(base_size = 22) +
+  theme(strip.text.y = element_text(angle = 0))
+
+pdf("~/spinal_cord_paper/figures/Fig_5_BPoly10int_flat_DE_volplot.pdf", height = 10, width = 5)
+alpha + 
+  ggtitle("aes(alpha)")
+alpha + 
+  NoLegend()
+alpha_size + 
+  labs(alpha = "-log10(log2FC)\nthres = 20",
+                  size = "-log10(log2FC)\nthres = 20") +
+  ggtitle("aes(alpha, size)")
+alpha_size + 
+  NoLegend()
+dev.off()
